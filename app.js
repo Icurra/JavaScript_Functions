e512f7bfa1a4c24b267ab4c9fa04a9623de23cba
Icurra <icurrablade@gmail.com>
2022-06-24 11:18:05 -0700

    first commit

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(userName, age) {
+  let aboveSixteen = `Congrats ${userName}, you can drive!`;
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16`;
+  if (age >= 16) {
+    console.log(aboveSixteen);
+  } else if (age < 16) {
+    console.log(belowSixteen);
+  } else {
+    console.log("Invalid Number");
+  }
+}
